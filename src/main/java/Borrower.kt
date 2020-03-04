2761df2bc93ab5dc6c105a00d6dea0e35dd1dd20
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-03-04 19:49:56 +0000

    Adds checkOutBook method to Borrower class

--- src/main/java/Borrower.kt
+++ src/main/java/Borrower.kt
@@ -10,4 +10,12 @@ class Borrower (name: String) {
     fun addBookToCollection(book: Book) {
         collection.add(book)
     }
+
+    fun checkOutBook(bookTitle: String, library: Library) {
+        val book: Book? = library.findBookByTitle(bookTitle)
+        if (book != null) {
+            library.removeBookFromCollection(book)
+            addBookToCollection(book)
+        }
+    }
 }
--- src/test/java/BorrowerTest.kt
+++ src/test/java/BorrowerTest.kt
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Assertions.*
 
 internal class BorrowerTest {
 
+    val library = Library(20)
     val borrower = Borrower("Gregor")
     val book1 = Book("The Martian Chronicles", "Ray Bradbury", "Sci-Fi")
+    val book2 = Book("The Grapes Of Wrath", "John Steinbeck", "Classic")
+    val book3 = Book("Jamie's Italian", "Jamie Oliver", "Cookery")
 
     @Test
     fun getName() {
@@ -21,6 +24,28 @@ internal class BorrowerTest {
     fun canAddBookToCollection() {
         borrower.addBookToCollection(book1)
         assertEquals(1, borrower.countCollection())
+    }
+
+    @Test
+    fun canCheckBookOutFromLibrary() {
+        library.addBookToCollection(book1)
+        library.addBookToCollection(book2)
+        library.addBookToCollection(book3)
+        borrower.checkOutBook("The Martian Chronicles", library)
+        assertEquals(1, borrower.countCollection())
+        assertEquals(2, library.countBooksInCollection())
+        assertTrue(borrower.collection[0] == book1)
+        assertFalse(library.stock.contains(book1))
+    }
+
+    @Test
+    fun cannotCheckBookOutIfNotFound() {
+        library.addBookToCollection(book1)
+        library.addBookToCollection(book2)
+        library.addBookToCollection(book3)
+        borrower.checkOutBook("Jane Eyre", library)
+        assertEquals(0, borrower.countCollection())
+        assertEquals(3, library.countBooksInCollection())
     }
 
 }
